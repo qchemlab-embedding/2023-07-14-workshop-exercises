2d364a1d6045468251a0d7e71f03971fd6cf20dc
gosia olejniczak <gosia.olejniczak@gmail.com>
2023-07-12 11:07:48 +0200

    add dirac files

---
+++ dirac/energy-h2o_dc_hf_cc-pvdz/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH -J dirac_test
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=5GB
+#SBATCH --time=01:00:00 
+#SBATCH -A plgqctda2-cpu
+#SBATCH -p plgrid-testing
+#SBATCH --output="output.out"
+#SBATCH --error="error.err"
+
+inp=dc_hf_cc-pvdz.inp
+mol=h2o.xyz
+scratch=$SCRATCH/dirac_tests/workshops/workshop_14july2023
+mkdir -p $scratch
+
+cd $SLURM_SUBMIT_DIR
+
+srun /bin/hostname
+
+module purge
+module load dirac/23.0-intel-2021b
+pam-dirac --scratch=$scratch --noarch --mw=900 --aw=1900 --mpi=$SLURM_NPROCS --inp=$inp --mol=$mol
+
+
